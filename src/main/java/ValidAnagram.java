0e6a89b53688f676dea60aaa4628efff68f1ec03
Bingxu Liu <bingliu@paypal.com>
2020-12-21 11:15:22 +0800

    Anagram

--- src/main/java/ValidAnagram.java
+++ src/main/java/ValidAnagram.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 
 public class ValidAnagram {
 
+    /**
+     * https://leetcode-cn.com/problems/valid-anagram/
+     */
     public boolean isAnagram(String s, String t) {
         char[] chars = s.toCharArray();
         char[] chart = t.toCharArray();
