7c98be33403accbaee6797da103cef4c2bed290e
Bingxu Liu <bingliu@paypal.com>
2020-12-21 11:13:19 +0800

    Anagram

--- src/main/java/ValidAnagram.java
+++ src/main/java/ValidAnagram.java
@@ -1,27 +1,13 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 
 public class ValidAnagram {
 
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> tmp = new LinkedHashMap<>();
-        for (Character c: s.toCharArray()){
-            if(tmp.get(c)!= null){
-                tmp.put(c,tmp.get(c)+1);
-            }else {
-                tmp.put(c,1);
-            }
-        }
-        for (Character c:t.toCharArray()){
-            if(tmp.get(c)== null ||tmp.get(c) == 0){
-                return false;
-            } else if(tmp.get(c) == 1) {
-                tmp.remove(c);
-            } else {
-                tmp.put(c,tmp.get(c)-1);
-            }
-        }
-        return tmp.size()==0;
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
+        Arrays.sort(chars);
+        Arrays.sort(chart);
+        return Arrays.equals(chars,chart);
     }
 }
